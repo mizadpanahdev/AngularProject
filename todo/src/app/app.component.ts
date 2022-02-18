c77dd196662548e0921355366db32ef9a194cf59
m.izadpanah <mohammad.saein@gmail.com>
2022-02-18 22:59:29 +0330

    add project

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -9,6 +9,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  list = new TodoList("محمد",[
+    new TodoItem("گرفتن ویزا اتریش", false),
+    new TodoItem("یادگیری git",true),
+    new TodoItem ("یادگیری زبان angular", false)
+  ])
+  get username():string{
+   return this.list.user;
+  };
+
+  get itemCount():number {
+    return this.list.items
+           .filter(item=>!item.complete).length
+  }
+  get items(){
+    return this.list.items.filter(item=>this.showcomplete || !item.complete)
+  }
+
+  addItem(newitem:string){
+    if (newitem!=""){
+      this.list.addItem(newitem);
+    }
+  }
+  showcomplete:boolean=false;
 }
 
