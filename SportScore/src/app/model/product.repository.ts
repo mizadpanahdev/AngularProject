d646d8ea1bb28f1f8c056aab396c5f5edf956753
m.izadpanah <mohammad.saein@gmail.com>
2022-03-13 00:38:29 +0330

    add store component

--- SportScore/src/app/model/product.repository.ts
+++ SportScore/src/app/model/product.repository.ts
@@ -1,5 +1,26 @@
 import { staticdatasource } from './static.datasource';
 import { razmanProduct } from './product.model';
 import { Injectable } from "@angular/core";
-import { Observable,from } from "rxjs";
+@Injectable()
+export class productRepository {
+private products:razmanProduct[] = [];
+private categories: string[] = [];
+constructor(private dataSource: staticdatasource) {
+dataSource.getProducts().subscribe(data => {
+this.products = data;
+this.categories = data.map(p => p.category)
+                      .filter((c, index, array) => array.indexOf(c) == index).sort();
+});
+}
+getProducts(category: string ): razmanProduct[] {
+    return this.products
+    .filter(p => category == null || category == p.category);
+    }
+    getProduct(id: number): razmanProduct{
+    return this.products.find(p => p.id == id);
+    } 
+    getCategories(): string[] {
+    return this.categories;
+    }
+}
 
